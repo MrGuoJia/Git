39d2a7dcf0f1fd4682fd34c907140ed540dd83d0
MrGuo <2270208063@qq.com>
2018-07-01 11:50:08 +0800

    second commit

--- app/src/main/java/com/example/jia/git/MainActivity.java
+++ app/src/main/java/com/example/jia/git/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
+        initView();/*不要在我这里写东西*/
     }
 
     private void initView() {
