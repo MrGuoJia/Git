487f42f5d7b82901b5150e004ad405bc70af9f70
MrGuo <2270208063@qq.com>
2018-07-01 11:51:45 +0800

    second commit

--- app/src/main/java/com/example/jia/git/MainActivity.java
+++ app/src/main/java/com/example/jia/git/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jia.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();/*不要在我这里写东西*/
+        Toast.makeText(this,"",Toast.LENGTH_LONG).show();
     }
 
     private void initView() {
