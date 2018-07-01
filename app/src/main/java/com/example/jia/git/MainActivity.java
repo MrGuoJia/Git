72047cb5eea67bdd4e1264d5028a50b5488215b0
MrGuo <2270208063@qq.com>
2018-07-01 12:03:19 +0800

    Merge branches 'dev' and 'master' of https://github.com/MrGuoJia/Git
    # Conflicts:
    #	app/src/main/java/com/example/jia/git/MainActivity.java

--- app/src/main/java/com/example/jia/git/MainActivity.java
+++ app/src/main/java/com/example/jia/git/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jia.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        Toast.makeText(this,"2",Toast.LENGTH_LONG).show();
     }
 
     private void initView() {
